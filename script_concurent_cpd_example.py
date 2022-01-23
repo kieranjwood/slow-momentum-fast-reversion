5cb3c6e1aa698351c8d5bfce36d74032d52a19f3
Kieran Wood <kieran.wood@eng.ox.ac.uk>
2022-01-23 23:13:25 +0000

    updated README

--- script_concurent_cpd_example.py
+++ script_concurent_cpd_example.py
@@ -9,7 +9,7 @@ if not os.path.exists(CPD_QUANDL_OUTPUT_FOLDER_DEFAULT):
     os.mkdir(CPD_QUANDL_OUTPUT_FOLDER_DEFAULT)
 
 all_processes = [
-    f'python script_cpd_example.py "{ticker}" "{os.path.join(CPD_QUANDL_OUTPUT_FOLDER_DEFAULT, ticker + ".csv")}" "2000-01-01" "2019-12-31"'
+    f'python script_cpd_example.py "{ticker}" "{os.path.join(CPD_QUANDL_OUTPUT_FOLDER_DEFAULT, ticker + ".csv")}" "1990-01-01" "2019-12-31"'
     for ticker in QUANDL_TICKERS
 ]
 process_pool = multiprocessing.Pool(processes=N_WORKERS)
